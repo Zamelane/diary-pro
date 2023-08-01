5d1119659206b0c454c7382d9dbe8e40c3822a95
Zamelane <39529518+Zamelane@users.noreply.github.com>
2023-08-01 20:54:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=no">
+    <title>Моя страница</title>
+  </head>
+  <script>
+    location.href = 'https://pro.diary.keenetic.link/'
+  </script>
+  <body>
+    <noscript>
+      <strong>Извините, ваш браузер не поддерживает скрипты</strong>
+    </noscript>
+  </body>
+</html>
